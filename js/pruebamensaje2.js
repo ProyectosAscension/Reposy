dc10330daa084d7e1c03e5c231c0c64cdca9f128
Adrian Gonzalez <adriangonzalez1837@gmail.com>
2021-01-15 13:14:40 -0600

    actualización pruebamensaje2, errorLogin

--- js/pruebamensaje2.js
+++ js/pruebamensaje2.js
@@ -325,3 +325,56 @@ $("#pedido6").click(function(){
       })
 
 });
+$("#pedido_prueba").click(function(){
+    const inputOptions = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({
+            'chica': 'Chica',
+            'mediana': 'Mediana',
+            'grande': 'Grande',
+            'extra': 'Extra Grande'
+          })
+        }, 1000)
+      })
+      
+      const { value: tamanio } = Swal.fire({
+        title: 'Seleccione tamaño de su Pizza:<br>Pepperoni',
+        input: 'radio',
+        showCancelButton: true,
+        cancelButtonColor: '#d33',
+        inputOptions: inputOptions,
+        cancelButtonText:'Cancelar Compra',
+        confirmButtonText:'Añadir al pedido',
+        footer:'Costos: <br>Chica $130.00 MXN <br>Mediana $150.00 MXN <br>Grande $170.00 MXN <br>Extra Grande $190.00 MXN <br>El tiempo de su pedido sera estimado en la seccion de Orden',
+        allowOutsideClick:false,
+        allowEscapeKey:false,
+        allowEnterKey:false,
+        stopKeydpwnPropagation:false,
+        inputValidator: (value) => {
+          if (!value) {
+            return 'Debe escoger un tamaño de pizza a ordenar'
+          }
+         else{
+              if(value=='chica')
+              {
+                return Swal.fire({ title:'Compra Hecha',text:'Su compra se ha agregado a su pedido',icon:'success',html: 'Tamaño de pizza seleccionado: Chica' })
+              }
+              if(value=='mediana')
+              {
+                return Swal.fire({ title:'Compra Hecha',text:'Su compra se ha agregado a su pedido',icon:'success',html: 'Tamaño de pizza seleccionado: Mediana' })
+              }
+              if(value=='grande')
+              {
+                return Swal.fire({ title:'Compra Hecha',text:'Su compra se ha agregado a su pedido',icon:'success',html: 'Tamaño de pizza seleccionado: Grande' })
+              }
+              if(value=='extra')
+              {
+                return Swal.fire({ title:'Compra Hecha',text:'Su compra se ha agregado a su pedido',icon:'success',html: 'Tamaño de pizza seleccionado: Extra Grande' })
+              }
+              return ''
+          }
+        }
+        
+      })
+
+});
