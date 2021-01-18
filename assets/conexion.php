94a7463d478c973a32160b6c19efbd719878c48e
AdrianGonzalez-17 <77258063+AdrianGonzalez-17@users.noreply.github.com>
2021-01-18 14:17:40 -0600

    Add files via upload

---
+++ assets/conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+    $dbhost="localhost";
+    $dbuser="root";
+    $dbpass="";
+    $dbname="pizzeria_asuncion";
+    $conn=mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+    if(!$conn)
+    {
+        die("No hay conexion: ".mysqli_connect_error());
+    }
+?>
